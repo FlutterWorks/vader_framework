ebe25b69c4d657caf4f6dc22e34cb8a2261da4d3
Martin Jablečník <martin.jablecnik@email.cz>
2025-01-27 18:07:41 +0100

    wip: add design_builder project

---
+++ cli/design_builder/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:design_builder/arguments.dart';
+import 'package:vader_cli/vader_cli.dart';
+
+void main(List<String> args) {
+  runCliApp(
+    arguments: args,
+    commands: commands,
+    parser: ExampleArguments.parse,
+    appInfo: AppInfo(
+      name: "design_builder",
+      version: '0.1.0',
+      description: "Builder for build directory structure in vader design package.",
+    ),
+    app: (args) {
+      print("Main part of my app...");
+      print("Message: ${args.message}");
+    },
+  );
+}
