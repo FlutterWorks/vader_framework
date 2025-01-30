96d641ebbf9ed1ebebc9e5d1b3961ea5124b9285
Martin Jablečník <martin.jablecnik@email.cz>
2025-01-30 18:23:48 +0100

    refactor: update ExampleButtonStyle and ExampleButtonStory

--- example_design/src/design/buttons/example_button/example_button.style.dart
+++ example_design/src/design/buttons/example_button/example_button.style.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:theme_tailor_annotation/theme_tailor_annotation.dart';
 
+@tailorMixinComponent
 class ExampleButtonStyle {
   const ExampleButtonStyle({
     required this.color,
