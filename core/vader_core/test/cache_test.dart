bb83d91099e821376be6eb1085c082804a378971
Martin Jablečník <martin.jablecnik@email.cz>
2025-02-10 22:16:44 +0100

    mock(vader_core): update cache tests

--- core/vader_core/test/cache_test.dart
+++ core/vader_core/test/cache_test.dart
@@ -1,11 +1,13 @@
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 import 'package:vader_core/clients/cache_client.dart';
+import 'package:vader_core/clients/storage_client.dart';
 
 performantProcess() {
   print('Running performant process...');
   return Future.delayed(
-    Duration(seconds: 5),
-        () => {'payload': 'Computed value'},
+    Duration(seconds: 3),
+    () => {'payload': 'Computed value'},
   );
 }
 
@@ -13,31 +15,45 @@ void main() {
   test('Cache save', () async {
     print('\nCache init:');
 
-    final cache = Cache();
+    final storageClient = StorageClientMock();
+    const key = 'random_key';
+
+    when(() => storageClient.saveMap(key, any())).thenAnswer(
+      (i) => Future.value(),
+    );
+
+    when(() => storageClient.getMap(key)).thenAnswer(
+      (i) => Future.value({
+        'time': DateTime.now().subtract(Duration(seconds: 11)).millisecondsSinceEpoch,
+        'data': {'payload': 'Cached value'},
+      }),
+    );
+
+    final cache = Cache(storageClient: storageClient);
     await Future.delayed(Duration(seconds: 1));
 
     print('\nRun process');
     var result = await cache.get(
-      key: 'random_key',
+      key: key,
       duration: Duration(seconds: 10),
       process: () => performantProcess(),
     );
     expect(result, {'payload': 'Computed value'});
 
-    print('\nRun process again');
+    print('\nObtain data from cache');
     result = await cache.get(
-      key: 'random_key',
-      duration: Duration(seconds: 10),
+      key: key,
+      duration: Duration(seconds: 12),
       process: () => performantProcess(),
     );
-    expect(result, {'payload': 'Computed value'});
-    print('Obtain data from cache');
+    expect(result, {'payload': 'Cached value'});
+    print('Data obtained from cache');
 
-    await Future.delayed(Duration(seconds: 2));
+    await Future.delayed(Duration(seconds: 10));
 
-    print('\nRun process after cache delay');
+    print('\nObtain data after cache delay');
     result = await cache.get(
-      key: 'random_key',
+      key: key,
       duration: Duration(seconds: 1),
       process: () => performantProcess(),
     );
