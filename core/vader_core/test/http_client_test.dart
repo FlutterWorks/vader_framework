c2b6f4a3a2274ada6fe81d2c432f023e589590c2
Martin Jablečník <martin.jablecnik@email.cz>
2025-02-10 01:29:45 +0100

    test(vader_core): add test for fetch method of HttpClient

--- core/vader_core/test/http_client_test.dart
+++ core/vader_core/test/http_client_test.dart
@@ -9,14 +9,24 @@ void main() {
   setupInjector(
     httpClient: HttpClient(apiUrl: "https://dummyjson.com", enableLogs: true, preventLargeResponses: false),
     storageClient: StorageClient(path: Directory.systemTemp.path),
-  );
+  ).commit();
+
   final httpClient = repositoryInjector.get<HttpClient>();
 
-  test('HttpClient', () async {
-    print('\nHttpClient:');
+  test('HttpClient request', () async {
+    print('\nHttpClient request:');
     final response = await httpClient.request(
       path: '/test',
       method: HttpMethod.get,
+    );
+    final result = jsonEncode(response.data).toString();
+    expect(result, '{"status":"ok","method":"GET"}');
+  });
+
+  test('HttpClient fetch', () async {
+    print('\nHttpClient fetch:');
+    final response = await httpClient.fetch(
+      path: '/test',
     );
     final result = jsonEncode(response.data).toString();
     expect(result, '{"status":"ok","method":"GET"}');
