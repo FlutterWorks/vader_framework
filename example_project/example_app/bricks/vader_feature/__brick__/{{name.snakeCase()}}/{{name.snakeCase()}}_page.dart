be881d0161e0bd65a843638d733bb0d1fb53b315
Martin Jablečník <martin.jablecnik@email.cz>
2025-03-24 12:02:45 +0100

    build(example_app): add go_route_builder into vader_feature brick

--- example_project/example_app/bricks/vader_feature/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_page.dart
+++ example_project/example_app/bricks/vader_feature/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_page.dart
@@ -1,5 +1,16 @@
 import 'package:{{package.snakeCase()}}_design/{{package.snakeCase()}}_design.dart';
+import 'package:vader_app/vader_app.dart';
 import 'package:flutter/material.dart';
+
+part '{{name.snakeCase()}}_page.g.dart';
+
+@TypedGoRoute<{{name.pascalCase()}}Route>(path: '/{{name.pathCase()}}')
+class {{name.pascalCase()}}Route extends GoRouteData {
+  const {{name.pascalCase()}}Route();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) => const {{name.pascalCase()}}Page();
+}
 
 class {{name.pascalCase()}}Page extends StatelessWidget {
   const {{name.pascalCase()}}Page({super.key});
