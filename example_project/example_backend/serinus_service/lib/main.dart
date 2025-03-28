c4818f55f413570c30378d8187a2838efe37bb0b
Martin Jablečník <martin.jablecnik@email.cz>
2025-03-28 22:01:36 +0100

    build(example_backend): add Dockerfile and flavors

--- example_project/example_backend/serinus_service/lib/main.dart
+++ example_project/example_backend/serinus_service/lib/main.dart
@@ -4,7 +4,9 @@ import 'package:hotreloader/hotreloader.dart';
 import 'app.dart';
 
 Future<void> main(List<String> arguments) async {
-  await HotReloader.create(debounceInterval: Duration(milliseconds: 300));
+  String flavor = const String.fromEnvironment('FLAVOR', defaultValue: 'devel');
+  if (flavor == 'devel') await HotReloader.create(debounceInterval: Duration(milliseconds: 300));
+
   final app = await serinus.createApplication(entrypoint: AppModule(), host: '0.0.0.0', port: 3000);
   await app.serve();
 }
