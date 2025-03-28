858a2efad9eaeae251746ce4952bc4c75f4f0d56
Martin Jablečník <martin.jablecnik@email.cz>
2025-03-28 16:34:14 +0100

    refactor(example_backend): remove auth module

--- example_project/example_backend/serinus_service/lib/app.dart
+++ example_project/example_backend/serinus_service/lib/app.dart
@@ -1,10 +1,9 @@
 import 'package:serinus/serinus.dart';
-import 'package:serinus_service/auth/auth_module.dart';
 import 'package:serinus_service/todo/todo_module.dart';
 
 class AppModule extends Module {
   AppModule() : super(
-    imports: [TodoModule(), AuthModule()],
+    imports: [TodoModule()],
     controllers: [],
     providers: [],
   );
