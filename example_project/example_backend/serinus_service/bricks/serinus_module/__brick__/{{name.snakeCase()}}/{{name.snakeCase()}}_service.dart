0f605e28db94f93001c28881c4bf63c3336fde98
Martin Jablečník <martin.jablecnik@email.cz>
2025-04-08 10:56:37 +0200

    hotfix(example_backend): update template

--- example_project/example_backend/serinus_service/bricks/serinus_module/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_service.dart
+++ example_project/example_backend/serinus_service/bricks/serinus_module/__brick__/{{name.snakeCase()}}/{{name.snakeCase()}}_service.dart
@@ -2,7 +2,7 @@ import 'package:serinus/serinus.dart';
 
 
 class {{name.pascalCase()}}Service extends Provider {
-  {{name.pascalCase()}}Service();
+  const {{name.pascalCase()}}Service();
 
   String getName(String name) {
     return "Hello $name!!";
