47f5be0b7f8c574a912da85af8e55ff9dcdd3336
Martin Jablečník <martin.jablecnik@email.cz>
2025-04-19 08:12:35 +0200

    refactor(example_backend): move AppModule into main.dart

--- example_project/example_backend/serinus_service/lib/main.dart
+++ example_project/example_backend/serinus_service/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:serinus/serinus.dart';
 import 'package:hotreloader/hotreloader.dart';
-
-import 'app.dart';
+import 'package:serinus_service/todo/todo_module.dart';
 
 Future<void> main(List<String> arguments) async {
   String flavor = const String.fromEnvironment('FLAVOR', defaultValue: 'devel');
@@ -9,4 +8,8 @@ Future<void> main(List<String> arguments) async {
 
   final app = await serinus.createApplication(entrypoint: AppModule(), host: '0.0.0.0', port: 3000);
   await app.serve();
+}
+
+class AppModule extends Module {
+  AppModule() : super(imports: [TodoModule()], controllers: [], providers: []);
 }
