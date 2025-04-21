65f2e6503344caea78528d6f6d9b0b650b449a72
Martin Jablečník <martin.jablecnik@email.cz>
2025-04-21 18:16:47 +0200

    build(example): add mason into setup project script

--- example_project/setup_project.sh
+++ example_project/setup_project.sh
@@ -15,6 +15,8 @@ fi
 
 ./rename_project.sh $old_name $new_name
 
+dart pub global activate mason_cli
+
 cd ${new_name}_design
 rm -rf pubspec_overrides.yaml
 task update-dependencies
@@ -35,15 +37,20 @@ rm -rf pubspec_overrides.yaml
 task update-dependencies
 task build-translations
 task run-build-runner
+mason add vader_entity --path bricks/vader_entity
+mason add vader_feature --path bricks/vader_feature
 cd ..
 
 cd ${new_name}_backend/serinus_service
 rm -rf pubspec_overrides.yaml
 task update-dependencies
 task run-build-runner
+mason add serinus_module --path bricks/serinus_module
+mason add serinus_model --path bricks/serinus_model
 cd ../..
 
 cd ${new_name}_backend/shelf_service
 rm -rf pubspec_overrides.yaml
 task update-dependencies
+mason add vader_component --path bricks/vader_component
 cd ../..
