1e06356e3205d565536c90185a84bffad61b69e2
Martin Jablečník <martin.jablecnik@email.cz>
2025-05-01 15:05:52 +0200

    feat(vader_core): add id generators

--- core/vader_core/lib/framework.dart
+++ core/vader_core/lib/framework.dart
@@ -13,3 +13,6 @@ export 'package:freezed_annotation/freezed_annotation.dart';
 export 'package:json_annotation/json_annotation.dart';
 export 'package:mocktail/mocktail.dart';
 
+export 'package:ulid/ulid.dart';
+export 'package:uuid/uuid.dart';
+export 'package:nanoid2/nanoid2.dart';
