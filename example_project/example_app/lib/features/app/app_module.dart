f5fde5aae65d20769940124ac4b686bcfea60e7c
Martin Jablečník <martin.jablecnik@email.cz>
2025-05-19 12:33:03 +0200

    fix(example_app): storageClient initialization

--- example_project/example_app/lib/features/app/app_module.dart
+++ example_project/example_app/lib/features/app/app_module.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:example_app/config.dart';
 import 'package:example_app/features/app/pages/initial_page.dart';
 import 'package:example_app/features/app/pages/error_page.dart';
+import 'package:flutter/foundation.dart';
 import 'package:vader_app/vader_app.dart';
 
 class AppModule extends VaderModule {
@@ -12,7 +15,7 @@ class AppModule extends VaderModule {
     final appConfig = AppConfig();
     return Injector()
       ..addInstance(appConfig)
-      ..addInstance(HttpClient(apiUrl: appConfig.apiUrl, enableLogs: true, preventLargeResponses: true))
-      ..addInstance(StorageClient());
+      ..addInstance(HttpClient(apiUrl: appConfig.apiUrl, enableLogs: true, preventLargeResponses: true, kIsWeb: kIsWeb))
+      ..addInstance(StorageClient(path: kIsWeb ? null : Directory.systemTemp.path));
   }
 }
